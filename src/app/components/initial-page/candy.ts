86a9200285186a22f78ac7b2a6d57017f5a49229
Raí joia miquilino valencio <79546241+Raijoia@users.noreply.github.com>
2024-04-05 19:39:09 +0000

    feat: add logic for candy machine

---
+++ src/app/components/initial-page/candy.ts
@@ -0,0 +1,4 @@
+export interface ICandy {
+    name: string;
+    value: number;
+}
--- src/app/components/initial-page/initial-page.component.ts
+++ src/app/components/initial-page/initial-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ICandy } from './candy';
 
 @Component({
   selector: 'app-initial-page',
@@ -8,5 +9,99 @@ import { Component } from '@angular/core';
   styleUrl: './initial-page.component.scss'
 })
 export class InitialPageComponent {
+  candy: ICandy[] = [
+    {
+      name: "crunch",
+      value: 5
+    },
+    {
+      name: "snickers",
+      value: 6
+    },
+    {
+      name: "twix",
+      value: 7
+    }
+  ]
+  values = [1, 2, 5]
+  valueAll = 0
+  ngOnInit() {}
 
+  addOneValue(): void {
+    if (this.valueAll == 10 || this.valueAll == 10) {
+      alert("Maximun value is 10!")
+      return;
+    }
+    this.valueAll += 1
+    console.log(this.valueAll)
+  }
+
+  addTwoValue(): void {
+    if (this.valueAll == 9 || this.valueAll == 10) {
+      alert("Maximun value is 10!")
+      return;
+    }
+    this.valueAll += 2
+    console.log(this.valueAll)
+  }
+
+  addFiveValue(): void {
+    if (this.valueAll == 6 || this.valueAll == 10) {
+      alert("Maximun value is 10!")
+      return;
+    }
+    this.valueAll += 5
+    console.log(this.valueAll)
+  }
+
+  buySelectOne(): void {
+    if (this.valueAll < 5) {
+      alert(`You can't buy this item, you value is ${this.valueAll}`)
+      return;
+    }
+    if (this.valueAll == 5) {
+      this.valueAll -= 5
+      alert(`Congratulations, you buy a crunch`)
+      return;
+    }
+    if (this.valueAll > 5) {
+      alert(`Congratulations, you buy a crunch and you change is ${this.valueAll - 5}`)
+      this.valueAll = 0
+      return;
+    }
+  }
+
+  buySelectTwo(): void {
+    if (this.valueAll < 5) {
+      alert(`You can't buy this item, you value is ${this.valueAll}`)
+      return;
+    }
+    if (this.valueAll == 6) {
+      this.valueAll -= 6
+      alert(`Congratulations, you buy a snickers`)
+      return;
+    }
+    if (this.valueAll > 6) {
+      alert(`Congratulations, you buy a snickers and you change is ${this.valueAll - 6}`)
+      this.valueAll = 0
+      return;
+    }
+  }
+
+  buySelectThree(): void {
+    if (this.valueAll < 5) {
+      alert(`You can't buy this item, you value is ${this.valueAll}`)
+      return;
+    }
+    if (this.valueAll == 7) {
+      this.valueAll -= 7
+      alert(`Congratulations, you buy a twix`)
+      return;
+    }
+    if (this.valueAll > 7) {
+      alert(`Congratulations, you buy a twix and you change is ${this.valueAll - 7}`)
+      this.valueAll = 0 
+      return;
+    }
+  }
 }
